731d4cdbad761545d671e362c08471d2e3b359ac
7UR7L3 <m7ur7l3@yahoo.com>
2017-03-12 14:15:51 -0600

    Included Dev Console "Script"

---
+++ tools/guestSearchSubjectGrabHelper.js
@@ -0,0 +1,2 @@
+var s = ""; document.querySelectorAll( "a.PSHYPERLINK" ).forEach( function( a ){ s += a.innerText + "\n"; } ); prompt( "cp me", s );
+	/* just run this for every letter; can format later with regex */
