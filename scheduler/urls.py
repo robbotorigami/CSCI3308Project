2b8079ef2f161951f85098e9cad0616cf3d70389
Robert Belter <robbotorigami@gmail.com>
2017-04-16 13:13:15 -0600

    Fixed urls

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'scheduler'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^addcourse/$', views.addcourse, name='addcourse')
+    url(r'^addcourse/$', views.addcourse, name='addcourse'),
     url(r'^addsection/$', views.addsection, name='addsection')
 ]
